28e925f7da8bc0126854d8ab6854d9635b8d87f7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-06 21:41:29 -0500

    Feat: added Guest and RegisteredCustomer classes.

---
+++ src/Model/Guest.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Guest extends User {
+
+    public Guest(int id, String firstName, String lastName, String email) {
+        super(id, firstName, lastName, email);
+    }
+
+}
