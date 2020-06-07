abda228cf4438f112c904b1bf1ad909fd527e4a8
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-06 21:33:21 -0500

    Feat: created User class in the model package.

---
+++ src/Model/User.java
@@ -0,0 +1,48 @@
+package Model;
+
+public abstract class User {
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+
+    public User(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
